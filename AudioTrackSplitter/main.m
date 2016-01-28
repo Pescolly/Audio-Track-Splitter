f37c6c2f115179d4e6f565cc53ab00c38939010f
Armen Karamian <akaramian@tv-met-mac-3.modern.mvfinc.com>
2016-01-28 11:22:15 -0800

    Initial Commit

---
+++ AudioTrackSplitter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AudioTrackSplitter
+//
+//  Created by Armen Karamian on 1/28/16.
+//  Copyright © 2016 Armen Karamian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
