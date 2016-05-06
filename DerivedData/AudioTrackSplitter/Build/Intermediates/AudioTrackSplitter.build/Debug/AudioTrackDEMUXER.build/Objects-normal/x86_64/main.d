55c40219e4d2d5e547f88583d20b427d96b6ada8
armen <absoboll@gmail.com>
2016-05-05 23:24:53 -0700

    Latest version for NBCU Spec.

---
+++ DerivedData/AudioTrackSplitter/Build/Intermediates/AudioTrackSplitter.build/Debug/AudioTrackDEMUXER.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/Ingest/06-metadata-creation/creation_personal_work_folders/Armen_Karamian/Codes/xcode/AudioTrackSplitter/AudioTrackSplitter/main.m \
+  /Volumes/Ingest/06-metadata-creation/creation_personal_work_folders/Armen_Karamian/Codes/xcode/AudioTrackSplitter/AudioTrackSplitter/SDAVAssetExportSession.h
